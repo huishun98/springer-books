673c5e5fdb1e6dd6b9e979c5dcfed62af688f8bb
Chua Hui Shun <huishun@Chuas-MacBook-Pro.local>
2020-05-07 16:42:42 +0800

    first commit

---
+++ src/components/Books.vue
@@ -0,0 +1,33 @@
+<script>
+import json from "../json/Free+English+textbooks+with+download_url.json";
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      publicPath: process.env.BASE_URL,
+      json: json,
+      selected: []
+    };
+  },
+  methods: {
+    select(i) {
+      this.selected.includes(i)
+        ? this.selected.splice(this.selected.indexOf(i), 1)
+        : this.selected.push(i);
+    },
+    openItems() {
+      let data_list = [];
+      for (let i = 0; i < this.selected.length; i++) {
+        const data = this.json[this.selected[i]]
+        const downloadUrl = data["Download URL"];
+        window.open(downloadUrl)
+      }
+    }
+  }
+};
+</script>
+
+<style scoped src="./books.css"></style>
+<template src="./books.html"></template>
+
